1a8f1328266c1cf65aad14495fe5da56533cadb4
ediwaldoneto <ediwaldo.neto@outlook.com>
2020-05-03 07:21:43 -0300

    Push exercicio

--- src/aula06/Pilha.java
+++ src/aula06/Pilha.java
@@ -1,50 +1,49 @@
 package aula06;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Pilha {
 
 	private Nodo topo;
 	private int tamanho;
-	private List<String> nomes = new LinkedList<String>();
-	
+	private String[] elemento;
+
 	public Pilha() {
 		this.topo = null;
 		this.tamanho = 0;
 	}
-	
-		
+
 	public void exibePilha() {
 		Nodo aux = topo;
-		String elementos="";
-		
-		while(aux != null) {
+		String elementos = "";
+
+		while (aux != null) {
 			elementos += aux.getNome() + " -> ";
 			aux = aux.getProximo();
 		}
 		System.out.println(elementos);
 	}
-	
+
 	public String getNomeTopo() {
 		return topo.getNome();
 	}
-	
-	public void insere(String elementos) {
-	    nomes.add(elementos);
-	    
-	}
-	
-	public String remove() {
-	    return nomes.remove(nomes.size()-1);
-	}
 
 	public boolean isEmpty() {
-		if(topo == null)
+		if (topo == null)
 			return true;
 		else
 			return false;
 	}
-	
-	
+
+	public void push(String nome) {
+		Nodo aux = topo;
+		topo = new Nodo();
+		topo.setNome(nome);
+		topo.setProximo(aux);
+
+	}
+
+	public void pop() {
+		
+				
+	}
+		
 }
--- src/aula06/TestePilha.java
+++ src/aula06/TestePilha.java
@@ -1,18 +1,23 @@
 package aula06;
 
+
+
 public class TestePilha {
 
 	public static void main(String[] args) {
+		
 		Pilha p = new Pilha();
+		p.push("JoAo");	   
+		p.push("MaRia");
+		p.push("JoSe");
+		p.push("Zezinho");
+		p.exibePilha();
+		p.pop();
 		
 		
-	    p.insere("joao");
+		//System.out.println("Elemento removido " + p.pop());
+		//System.out.println("Elemento removido " + p.pop());
 		
 		p.exibePilha();
-		
-		//System.out.println("Elemento removido " + p.pop());
-		//System.out.println("Elemento removido " + p.pop());
-		
-		//p.exibePilha();
 	}
 }
